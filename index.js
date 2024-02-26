b30cb9d1101f0dd9dd2db38877f12f74d557fe84
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-02-26 20:05:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+// var num=3
+// for(var i=1; i<=10; i++){
+//     console.log(num +"x"  + i + "=" +(num*i));
+
+
+// }
+var num = parseInt(prompt("Please Enter your number"));
+// var num=3
+for(var i=1; i<=10; i++){
+    document.write(num +"x"  + i + "=" +(num*i) + "</br>" )
+}
+
